2f62135a73364ba1ac1f29263259efd3b00e8760
shubham kumar <kumarshubham41515@gmail.com>
2024-11-29 03:39:28 +0530

    added links nav icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,30 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <nav className=" mb-15 flex items-centre justify-between py-6">
+        <nav className="mb-15 flex items-centre justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="logo" />
+                <a href="#top" className="mx-2">
+                    <img className="w-10" src={logo} alt="logo" />
+                </a>
             </div>
 
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaTwitter />
-                <FaInstagram />
+                <a href="https://www.linkedin.com/in/shubham-kumar-mishra/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="hover:text-blue-600" />
+                </a>
+
+                <a href="https://github.com/mishrashubhcode" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="hover:text-gray-800" />
+                </a>
+
+                <a href="https://x.com/mishraasshubham" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter className="hover:text-blue-400" />
+                </a>
+
+                <a href="https://www.instagram.com/mishraa_sshubham/" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className="hover:text-pink-500" />
+                </a>
+
             </div>
         </nav>
     )
